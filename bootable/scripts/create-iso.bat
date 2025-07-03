4dfe92d1b0c2fe7221d6de64aa3163d99a1cbeb7
Omar Minaya <omiinaya@gmail.com>
2025-07-02 23:58:58 -0400

    fuck this shit

--- bootable/scripts/create-iso.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0make-uefi.ps1"
-if errorlevel 1 (
-    echo Error creating ISO
-    pause
-    exit /b 1
-)
-echo ISO created successfully
-pause 
